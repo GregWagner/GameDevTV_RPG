7da4a7e05f2d1735276423424e9bd0127f5ee475
GregWagner <theGreg101@gmail.com>
2024-04-11 15:17:33 -0500

    Finished sandbox (Section 2)

---
+++ RPG Project/Assets/Scripts/Mover.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour {
+    [SerializeField] private Transform _target;
+
+    private void Update() {
+        if (Input.GetMouseButton(0)) {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator() {
+        // get the global velocity from the nav mesh agent
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+
+        // convert it to a local velocity
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+        // set animators blend value to be equal to  out desired
+        // forward speed (on the Z-axis)
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+
+    private void MoveToCursor() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit, 100f);
+        if (hasHit) {
+            var agent = GetComponent<NavMeshAgent>();
+            agent.destination = hit.point;
+        }
+        //Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
+    }
+}
