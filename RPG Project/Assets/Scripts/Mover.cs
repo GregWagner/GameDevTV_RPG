1b76673125b652b3ce3643370276ca4851ecf1ee
GregWagner <theGreg101@gmail.com>
2024-04-11 16:07:29 -0500

    separated the player control layer

--- RPG Project/Assets/Scripts/Mover.cs
+++ RPG Project/Assets/Scripts/Mover.cs
@@ -6,9 +6,6 @@ public class Mover : MonoBehaviour {
     [SerializeField] private Transform _target;
 
     private void Update() {
-        if (Input.GetMouseButton(0)) {
-            MoveToCursor();
-        }
         UpdateAnimator();
     }
 
@@ -25,14 +22,8 @@ public class Mover : MonoBehaviour {
         GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 
-    private void MoveToCursor() {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit, 100f);
-        if (hasHit) {
-            var agent = GetComponent<NavMeshAgent>();
-            agent.destination = hit.point;
-        }
-        //Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
+    public void MoveTo(Vector3 destination) {
+        var agent = GetComponent<NavMeshAgent>();
+        agent.destination = destination;
     }
 }
